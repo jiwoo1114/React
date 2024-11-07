fcc14b3f2fcaad3fda4095c47bb88e90192dc4f8
jiwoo1114 <isy0620@naver.com>
2024-11-07 11:04:36 +0900

    up

--- React_Quiz_Practice/ch11/src/Quiz01.js
+++ React_Quiz_Practice/ch11/src/Quiz01.js
@@ -6,10 +6,13 @@ const initialState = []
 // 리듀서 함수
 //state를 바꿔주는 함수
 function reducer(state, action) {
+   //state: 현재 state값(todos)
+   //action: dispath에서 전달한 매개변수 값 =>{type: 'add', payload:'저녁먹기'}
+   //저녁먹기 삭제할 때 action => {type: 'delete', payload:'2024-11-07 10:50:12'} 저녁먹기를 제외한 배열이 출력
    switch (action.type) {
-      case 'add': 
+      case 'add': // ...state:꼭 깊은복사를 해야 기존의데이터 유지하며 추가 가능 
          return [...state, {id:Date.now(), text: action.payload}]// 여기에 코드 작성
-      case 'delete':
+      case 'delete': 
          return  state.filter((todo) => todo.id !== action.payload)// 여기에 코드 작성
       default:
          return state
@@ -24,7 +27,9 @@ const Quiz01 = () => {
    // 새로운 할 일 추가
    const handleAddTodo = () => {
       if (inputValue.trim()) {
-         dispatch({ type: 'add', payload:  inputValue  })
+         //action을 받음,매개변수 2개 이상이어 json 객체로 받음
+         //dispath의 역할: action 전달,reducer 호출 
+         dispatch({ type: 'add', payload: inputValue })
          setInputValue('')
          //여기에 코드 작성
       }
@@ -32,7 +37,8 @@ const Quiz01 = () => {
 
    // 할 일 삭제
    const handleDeleteTodo = (id) => {
-      dispatch({ type: 'delete', payload: id})
+      //action를 받음
+      dispatch({ type: 'delete', payload: id })
       //여기에 코드 작성
    }
 
